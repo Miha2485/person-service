610f741d7f7c02adec7123e6a8d2884a4622e083
mihailpoliak <mihailpoliak@mbp-mihail>
2024-09-05 19:40:24 +0300

    Homework 12

--- src/main/java/telran/java53/person/controller/PersonController.java
+++ src/main/java/telran/java53/person/controller/PersonController.java
@@ -1,6 +1,5 @@
 package telran.java53.person.controller;
 
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,61 +18,53 @@ import telran.java53.person.service.PersonService;
 @RestController
 @RequestMapping("/person")
 @RequiredArgsConstructor
-public class PersonController  {
-	final PersonService personService;
+public class PersonController {
+    final PersonService personService;
 
+    @PostMapping
+    public Boolean addPesron(@RequestBody PersonDto personDto) {
+        return personService.addPerson(personDto);
+    }
 
-	@PostMapping
-	public Boolean addPesron(@RequestBody PersonDto personDto) {
-		return personService.addPerson(personDto);
-	}
+    @GetMapping("/{id}")
+    public PersonDto findPersonById(@PathVariable Integer id) {
+        return personService.findPersonById(id);
+    }
 
+    @DeleteMapping("/{id}")
+    public PersonDto removePerson(@PathVariable Integer id) {
+        return personService.removePerson(id);
+    }
 
-	@GetMapping("/{id}")
-	public PersonDto findPersonById(@PathVariable Integer id) {
-		return personService.findPersonById(id);
-	}
+    @PutMapping("/{id}/name/{name}")
+    public PersonDto updatePersoneName(@PathVariable Integer id, @PathVariable String name) {
+        return personService.updatePersoneName(id, name);
+    }
 
+    @PutMapping("/{id}/address")
+    public PersonDto updatePersoneAddress(@PathVariable Integer id, @RequestBody AddressDto addressDto) {
+        return personService.updatePersoneAddress(id, addressDto);
+    }
 
-	@DeleteMapping("/{id}")
-	public PersonDto removePerson(Integer id) {
-		return personService.removePerson(id);
-	}
+    @GetMapping("/city/{city}")
+    public PersonDto[] findPersonByCity(@PathVariable String city) {
+        return personService.findPersonByCity(city);
+    }
 
+    @GetMapping("/name/{name}")
+    public PersonDto[] findPersonByName(@PathVariable String name) {
+        return personService.findPersonByName(name);
+    }
 
-	@PutMapping("/{id}/name/{name}")
-	public PersonDto updatePersoneName(Integer id, String name) {
-		return personService.updatePersoneName(id, name);
-	}
+    @GetMapping("/ages/{min}/{max}")
+    public PersonDto[] findPersonBetweenAge(@PathVariable Integer min, @PathVariable Integer max) {
+        return personService.findPersonBetweenAge(min, max);
+    }
 
-
-	@PutMapping("/{id}/address")
-	public PersonDto updatePersoneAddress(Integer id, AddressDto addressDto) {
-		return personService.updatePersoneAddress(id, addressDto);
-	}
-
-
-	@GetMapping("/city/{city}")
-	public PersonDto[] findPersonByCity(String city) {
-		return personService.findPersonByCity(city);
-	}
-
-
-	@GetMapping("/name/{name}")
-	public PersonDto[] findPersonByName(@PathVariable String name) {
-	
-		return personService.findPersonByName(name);
-	}
-
-
-	@GetMapping("/ages/{min}/{max}")
-	public PersonDto[] findPersonBetweenAge(@PathVariable Integer min, Integer max) {
-		return personService.findPersonBetweenAge(min, max);
-	}
-
-
-	@GetMapping("/population/city")
-	public Iterable<CityPopulationDto> getCitiesPopulation() {
-		return personService.getCitiesPopulation();
-	}
+    @GetMapping("/population/city")
+    public Iterable<CityPopulationDto> getCitiesPopulation() {
+        return personService.getCitiesPopulation();
+    }
 }
+
+
--- src/main/java/telran/java53/person/service/PersonServiceImpl.java
+++ src/main/java/telran/java53/person/service/PersonServiceImpl.java
@@ -1,5 +1,9 @@
 package telran.java53.person.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +15,7 @@ import telran.java53.person.dto.CityPopulationDto;
 import telran.java53.person.dto.PersonDto;
 import telran.java53.person.dto.exception.PersonNotFoundException;
 import telran.java53.person.model.Person;
+import telran.java53.person.model.Address;
 
 @Transactional
 @Service
@@ -28,13 +33,11 @@ public class PersonServiceImpl implements PersonService {
 		return true;
 	}
 
-
 	@Override
 	public PersonDto findPersonById(Integer id) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 		return modelMapper.map(person, PersonDto.class);
 	}
-
 
 	@Override
 	public PersonDto removePerson(Integer id) {
@@ -43,39 +46,50 @@ public class PersonServiceImpl implements PersonService {
 		return modelMapper.map(person, PersonDto.class);
 	}
 
-
-
-
 	@Override
 	public PersonDto updatePersoneName(Integer id, String name) {
-		
-		return null;
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException());
+		person.setName(name);
+		personRepository.save(person);
+		return modelMapper.map(person, PersonDto.class);
 	}
-
 
 	@Override
 	public PersonDto updatePersoneAddress(Integer id, AddressDto addressDto) {
-	
-		return null;
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException());
+		person.setAddress(modelMapper.map(addressDto, Address.class));
+		personRepository.save(person);
+		return modelMapper.map(person, PersonDto.class);
 	}
 
 	@Override
 	public PersonDto[] findPersonByCity(String city) {
-		// TODO Auto-generated method stub
-		return null;
+	    return StreamSupport.stream(personRepository.findAll().spliterator(), false)
+	            .filter(person -> person.getAddress() != null && person.getAddress().getCity().equalsIgnoreCase(city))
+	            .map(person -> modelMapper.map(person, PersonDto.class))
+	            .toArray(PersonDto[]::new);
 	}
+
 
 	@Override
 	public PersonDto[] findPersonByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Person> people = personRepository.findByName(name);
+		return people.stream().map(person -> modelMapper.map(person, PersonDto.class)).toArray(PersonDto[]::new);
 	}
 
 	@Override
 	public PersonDto[] findPersonBetweenAge(Integer minAge, Integer maxAge) {
-		// TODO Auto-generated method stub
-		return null;
+	    LocalDate from = LocalDate.now().minusYears(maxAge);
+	    LocalDate to = LocalDate.now().minusYears(minAge);
+
+	    return StreamSupport.stream(personRepository.findAll().spliterator(), false)
+	            .filter(p -> p.getBirthDate() != null &&
+	                          p.getBirthDate().isAfter(from) &&
+	                          p.getBirthDate().isBefore(to))
+	            .map(p -> modelMapper.map(p, PersonDto.class))
+	            .toArray(PersonDto[]::new);
 	}
+
 
 	@Override
 	public Iterable<CityPopulationDto> getCitiesPopulation() {
